895663353a5244246f94128b9f9b9698bdb18b13
Jonathan <st190039@stud.uni-stuttgart.de>
2025-12-10 13:52:02 +0100

    Menge 'tau_distance_set' implementiert, deren Gitterphndie tau-Abstandsbedingunen

--- Clustering/cluster.py
+++ Clustering/cluster.py
@@ -14,9 +14,8 @@ args = vars(parser.parse_args())
 
 datasetname = args["datasetname"]
 delta = args["delta"]
-eps = args["eps_factor"]
-tau = args["tau_factor"]
-
+eps_factor = args["eps_factor"]
+tau_factor = args["tau_factor"]
 
 data_path = "cluster-data/" + datasetname + ".csv"
 result_path = "cluster-results/team-12-" + datasetname + ".result.csv"
@@ -26,14 +25,18 @@ result_log_path = "cluster-results/team-12-" + datasetname + ".result.log"
 df = pd.read_csv(data_path)
 data_list : list[list[float]] = df.to_numpy().tolist()
 
+####################################################################################################
+# Verarbeitung der Daten (ohne Verwendung der Module!)
+####################################################################################################
+
 # def cluster_analysis(data_list : list[list[float]]) -> list[list[float]]:
 #     return [[]]
 # clustered_data = pd.DataFrame(cluster_analysis(data_list))
 # clustered_data.to_csv(result_path, index=False)
 
-data_lattice_list : list[tuple[int]] = [tuple([int((component + 1) // delta)
-                                               for component in point])
-                                            for point in data_list]
+data_lattice_list : list[tuple] = [tuple([int((component + 1) // delta)
+                                          for component in point])
+                                    for point in data_list]
 
 density_dict = {}
 
@@ -53,18 +56,62 @@ for lattice_point in data_lattice_list:
     density_dict[lattice_point] = getDensity(lattice_point) + 1
 
 #rho_bar ist einfach nur step_count + 1, rho ist nur rho_bar mit Vorfaktor
-def survivingLatticePoints(step_count: int) -> set[tuple[int]]:
+def survivingLatticePoints(step_count: int) -> set[tuple]:
     return {lattice_point for lattice_point in data_lattice_list 
                 if density_dict[lattice_point] > step_count}
 
-for i in [1,5,10,50,100]:
-    tuple_set = survivingLatticePoints(i)
-    survived = [data_list[i] for i in range(len(data_list)) if data_lattice_list[i] in tuple_set]
-    xdata = [item[0] for item in survived]
-    ydata = [item[1] for item in survived]
-    print(xdata)
-    plt.scatter(xdata, ydata, s=10, alpha=0.5)
-    x = [(item[0]+.5)*delta - 1 for item in tuple_set]
-    y = [(item[1]+.5)*delta - 1 for item in tuple_set]
-    plt.scatter(x, y, s=50, alpha=0.3)
-    plt.show()
+# Am besten oben definieren:
+dim = len(data_list[0])
+# n_data = len(data_list)
+# density_factor = n_data * (2 ** dim) * (delta ** dim)
+# tau = tau_factor * delta
+
+def cartesian_product(X : list[list]):
+    dim = len(X)
+    prod = [()]
+    for l in range(dim):
+        prod = [ tup + (item,) for tup in prod for item in X[l] ]
+    return prod
+
+def cartesian_potentiation(lst : list, dim : int):
+    return cartesian_product([lst for _ in range(dim)])
+
+# Intervall [-(int(tau_factor)+1), ... , int(tau_factor)+1] Reicht das ??
+eff_tau_interval_list = list(range(-int(tau_factor)-1,int(tau_factor)+2))
+
+# dim-dimensionale Box, die alle Gitterpunkte enthält, für die eff_origin_distance<=tau_factor gelten könnte
+tau_box = cartesian_potentiation(eff_tau_interval_list, dim)
+
+def eff_origin_distance(lattice_point: tuple):
+    eff_lattice_point_list = [coord-1 if coord > 0 else coord+1 if coord < 0 else coord for coord in lattice_point]
+    eff_coord_squares = [coord**2 for coord in eff_lattice_point_list]
+    return sum(eff_coord_squares)**.5
+
+# dist(box1, box2) <= tau <=> eff_origin_distance() der Differenz der assoziierten lattice points <= tau_factor (= tau/delta) 
+tau_distance_set = {lattice_point for lattice_point in tau_box if eff_origin_distance(lattice_point) <= tau_factor}
+
+
+
+####################################################################################################
+# Visualisierung
+####################################################################################################
+
+# print(tau_factor)
+# lst = list(tau_distance_set)
+# x = [point[0] for point in lst]
+# y = [point[1] for point in lst]
+# plt.scatter(x, y)
+# plt.show()
+
+# if dim == 2:
+#     for i in [5]:
+#         tuple_set = survivingLatticePoints(i)
+#         survived = [data_list[i] for i in range(len(data_list)) if data_lattice_list[i] in tuple_set]
+#         xdata = [item[0] for item in survived]
+#         ydata = [item[1] for item in survived]
+#         # print(xdata)
+#         plt.scatter(xdata, ydata, s=10, alpha=0.5)
+#         x = [(item[0]+.5)*delta - 1 for item in tuple_set]
+#         y = [(item[1]+.5)*delta - 1 for item in tuple_set]
+#         plt.scatter(x, y, s=50, alpha=0.3)
+#         plt.show()
