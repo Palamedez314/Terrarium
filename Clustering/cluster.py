370bd9dcb4060ea8bc71e1ba892472e7dddd2501
Jonathan <st190039@stud.uni-stuttgart.de>
2025-12-13 19:50:59 +0100

    Ersten Zusammennhangskomponenten-Algorithmus implementiert

--- Clustering/cluster.py
+++ Clustering/cluster.py
@@ -29,28 +29,14 @@ data_list : list[list[float]] = df.to_numpy().tolist()
 # Verarbeitung der Daten (ohne Verwendung der Module!)
 ####################################################################################################
 
-# def cluster_analysis(data_list : list[list[float]]) -> list[list[float]]:
-#     return [[]]
-# clustered_data = pd.DataFrame(cluster_analysis(data_list))
-# clustered_data.to_csv(result_path, index=False)
-
 data_lattice_list : list[tuple] = [tuple([int((component + 1) // delta)
                                           for component in point])
                                     for point in data_list]
 
 density_dict = {}
 
-# def dictMaybeInside(dic,key):
-#     if key in dic:
-#         return dic[key]
-#     else:
-#         return 0
-
 def getDensity(lattice_point: tuple[int]) -> int:
-    if lattice_point in density_dict:
-        return density_dict[lattice_point]
-    else:
-        return 0
+    return density_dict.get(lattice_point, 0)
 
 for lattice_point in data_lattice_list:
     density_dict[lattice_point] = getDensity(lattice_point) + 1
@@ -90,6 +76,81 @@ def eff_origin_distance(lattice_point: tuple):
 # dist(box1, box2) <= tau <=> eff_origin_distance() der Differenz der assoziierten lattice points <= tau_factor (= tau/delta) 
 tau_distance_set = {lattice_point for lattice_point in tau_box if eff_origin_distance(lattice_point) <= tau_factor}
 
+def epsDensityTest(component: set[tuple], step_count: int) -> bool:
+    for lattice_point in component:
+        if density_dict[lattice_point] > step_count: #+ eps_bar: #ToDo : richtige Bedingung einfügen!!!
+            return True
+    return False
+
+def connectedComponents(step_count: int) -> list[set[tuple]]:
+    connected_component_list : list[set[tuple]] = []
+
+    for lattice_point in survivingLatticePoints(step_count):
+
+        # if step_count in [0]:
+        #     print(f"\nmerge {lattice_point} into\n{connected_component_list}")
+
+        neighbors = {tuple(map(sum, zip(lattice_point, diff))) for diff in tau_box}
+        # ist das schneller:? = [tuple([sum(components) for components in zip(latticePoint, diff)])
+        #                         for diff in tau_box]
+        neighboring_component_indices = [i for i in range(len(connected_component_list)) if bool(neighbors & connected_component_list[i])]
+
+        # if step_count in [0]:
+        #     print(bool(neighboring_component_indices))
+
+        if neighboring_component_indices: # aka wenn ... nichtleer
+
+            # if step_count in [0]:
+            #     print(neighboring_component_indices)
+
+            min_index = neighboring_component_indices.pop(0)
+            final_component = connected_component_list[min_index]
+            # # richtig rum funktioniert nicht weil Rest der Liste sich verschiebt:
+            # for i in neighboring_component_indices:
+            #     final_component.update(connected_component_list.pop(i))
+            for i in reversed(neighboring_component_indices):
+                final_component.update(connected_component_list.pop(i))
+            final_component.add(lattice_point)
+            
+            # # Alternative: (geht das deleten noch besser?)
+            # merge_components = [connected_component_list[i] for i in neighboring_component_indices]
+            # final_component.update(*merge_components)
+            # for i in reversed(neighboring_component_indices):
+            #     del connected_component_list[i]
+        else:
+            connected_component_list.append({lattice_point})
+
+        
+    # Wäre es besser ein dictionary statt der Liste "connected_component_list" zu benutzen?
+    
+    surviving_components = [component for component in connected_component_list if epsDensityTest(component,step_count)]
+    return surviving_components
+
+# woanders hin?
+step_count_limit = 10000
+
+step_count = 0
+connected_component_count = 1
+connected_component_list = []
+while connected_component_count == 1:
+    if step_count > step_count_limit:
+        raise RuntimeError(f"Clustering nach {step_count_limit} Versuchen abgebrochen")
+    
+    connected_component_list = connectedComponents(step_count)
+    connected_component_count = len(connected_component_list)
+    step_count += 1
+
+if connected_component_count == 0:
+    clustered_data = [[1] + point for point in data_list]
+
+print(step_count)
+print(connected_component_list)
+
+# else:
+    # ToDo:
+    # dictionary, dass Gitterpunkten ihre Zusammenhangskomponente zuweist
+    # Datenpunkte über zugeordnete Gitterpunkte mit Zusammenhangskomponente markieren
+    
 
 
 ####################################################################################################
@@ -103,15 +164,46 @@ tau_distance_set = {lattice_point for lattice_point in tau_box if eff_origin_dis
 # plt.scatter(x, y)
 # plt.show()
 
-# if dim == 2:
-#     for i in [5]:
-#         tuple_set = survivingLatticePoints(i)
-#         survived = [data_list[i] for i in range(len(data_list)) if data_lattice_list[i] in tuple_set]
-#         xdata = [item[0] for item in survived]
-#         ydata = [item[1] for item in survived]
-#         # print(xdata)
-#         plt.scatter(xdata, ydata, s=10, alpha=0.5)
-#         x = [(item[0]+.5)*delta - 1 for item in tuple_set]
-#         y = [(item[1]+.5)*delta - 1 for item in tuple_set]
-#         plt.scatter(x, y, s=50, alpha=0.3)
-#         plt.show()
+if dim == 2:
+    for i in [0]:
+        tuple_set = survivingLatticePoints(i)
+        survived = [data_list[i] for i in range(len(data_list)) if data_lattice_list[i] in tuple_set]
+        xdata = [item[0] for item in survived]
+        ydata = [item[1] for item in survived]
+        # print(xdata)
+        plt.scatter(xdata, ydata, s=10, alpha=0.5)
+        x = [(item[0]+.5)*delta - 1 for item in tuple_set]
+        y = [(item[1]+.5)*delta - 1 for item in tuple_set]
+        plt.scatter(x, y, s=50, alpha=0.3)
+        # first_cluster = connectedComponents(0)
+        # xfc = [(item[0]+.5)*delta - 1 for item in first_cluster[0]]
+        # yfc = [(item[1]+.5)*delta for item in first_cluster[0]]
+        # plt.scatter(xfc, yfc, s=20, alpha=0.7)
+        a_cluster = list(connected_component_list[0])
+        b_cluster = list(connected_component_list[1])
+        xa = [(item[0]+.5)*delta - 1 for item in a_cluster]
+        ya = [(item[1]+.5)*delta - 1 for item in a_cluster]
+        xb = [(item[0]+.5)*delta - 1 for item in b_cluster]
+        yb = [(item[1]+.5)*delta - 1 for item in b_cluster]
+        plt.scatter(xa, ya, s=40, alpha=0.7)
+        plt.scatter(xb, yb, s=40, alpha=0.7)
+
+        plt.show()
+
+
+####################################################################################################
+# Schreiben der Daten in .csv/.png-Dateien
+####################################################################################################
+
+# result_data_frame = pd.DataFrame(clustered_data(data_list))
+# result_data_frame.to_csv(result_path, index=False)
+
+
+
+# ToDo (global):
+# markieren der Daten mit Cluster-Indices durch else-Fall vervollständigen
+# epsDensityTest richtig definieren
+# log-Datei Daten implementieren
+# richtige Sachen in die richtigen Dateien schreiben
+# finale Datenvisualisierung für 2D
+# ...
